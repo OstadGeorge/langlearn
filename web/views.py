611c2e2286967839644e31afb6d27df517ba0476
mohammad ali heydari <aheydari831@gmail.com>
2019-07-14 16:44:13 +0430

    some change like adding route include and adding template.html and static files and ...

--- web/views.py
+++ web/views.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+# from django.http import JsonResponse
+# from json import JSONEncoder
+# from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
+    
