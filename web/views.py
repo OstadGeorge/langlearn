04c499cf0d80be32d81ae426a8e9f65aa1e5c00a
mohammad ali heydari <aheydari831@gmail.com>
2019-07-15 21:39:10 +0430

    not special changes :))

--- web/views.py
+++ web/views.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # from django.http import JsonResponse
 # from json import JSONEncoder
-# from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-    
+
+def template(req):
+    return render(req, 'template.html', context={"status":"ok"})
+    # HttpResponse("SALAM")
